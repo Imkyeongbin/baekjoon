2bb0facf2bde982955d0eeb5acfd15eda4bd72a5
Im Kyeongbin <fjeidjenk38@gmail.com>
2025-12-31 16:14:27 +0900

    10951 방법 우선 순위 전환

--- python/03/10951.py
+++ python/03/10951.py
@@ -1,12 +1,13 @@
 import sys
-input = sys.stdin.readline
-while True:
-    try:
-        a, b = map(int, input().split())
-    except:
-        break
+
+for i in sys.stdin:
+    a, b = map(int, i.split())
     print(a + b)
 
-# for i in sys.stdin:
-#     a, b = map(int, i.split())
+# input = sys.stdin.readline
+# while True:
+#     try:
+#         a, b = map(int, input().split())
+#     except:
+#         break
 #     print(a + b)
