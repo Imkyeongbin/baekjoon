c1a8db349adfdc783f2917002f73610970cf028a
Im Kyeongbin <fjeidjenk38@gmail.com>
2025-12-31 16:13:30 +0900

    10951 가능한 방법 하나 더

--- python/03/10951.py
+++ python/03/10951.py
@@ -2,7 +2,11 @@ import sys
 input = sys.stdin.readline
 while True:
     try:
-        a,b = map(int, input().split())
+        a, b = map(int, input().split())
     except:
         break
-    print(a+b)
+    print(a + b)
+
+# for i in sys.stdin:
+#     a, b = map(int, i.split())
+#     print(a + b)
